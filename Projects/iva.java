fae2940336874a85a993ee286472ffc982f10a5a
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-04-05 21:56:22 +0200

    Add files via upload

---
+++ Projects/iva.java
@@ -0,0 +1,60 @@
+/*
+ * Calculate the VAT of a price
+ */
+
+import java.util.Scanner;
+
+public class iva {
+
+ 
+ public static float calcIva(float x, float y) {
+
+        float z = (x * y) / 100;
+
+        float h = x + z;
+
+        return h;
+    }
+
+ public static void main(String[] args) {
+
+     
+      
+         float g;
+         float j;
+         float result;   
+         
+
+         System.out.println("Calcolo IVA");
+         System.out.println("Copyright Leo Corp");
+         System.out.println("");
+         System.out.println("Prezzo senza IVA: ");
+
+         Scanner var1 = new Scanner(System.in);
+         g = var1.nextFloat();
+
+         System.out.println("% IVA:");
+         Scanner var2 = new Scanner(System.in);
+         j = var2.nextFloat();
+
+         result = calcIva(g, j);
+
+         System.out.println();
+         System.out.println("Prezzo con IVA: " + result);
+          
+     
+    }
+     
+  }
+     
+     
+      
+     
+      
+      
+     
+    
+
+    
+
+
---
+++ Projects/percentage.java
@@ -0,0 +1,43 @@
+/*
+ * calculate the percentage of a number 
+ */
+
+import java.util.Scanner;
+
+public class percentage {
+ 
+ public static float perc(float x, float y) {
+  
+  return (x * y) / 100;
+ }
+
+ 
+ public static void main(String[]args ) {
+  
+  float g; 
+  float h;
+  float result;
+   
+  
+  System.out.println("Programma per la percentuale di due numeri.");
+  System.out.println("Copyright Leo Corp.");
+  System.out.println();
+  System.out.println("Inserisci il totale:");
+  
+  Scanner var1 = new Scanner(System.in);
+  g = var1.nextFloat();
+  
+  
+  
+  System.out.println("Inserisci la percentuale:");
+  Scanner var2 = new Scanner(System.in);
+  h=var2.nextFloat();
+  
+  result = perc(g, h);
+  
+  System.out.println();
+  System.out.println("Risultato: " + result);
+  
+ 
+ }
+}
