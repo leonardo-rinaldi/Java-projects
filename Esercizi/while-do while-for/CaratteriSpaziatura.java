31184dc5f0396979c7f59f4560c82f09c593caee
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-07-07 17:19:57 +0200

    Add files via upload

---
+++ Esercizi/while-do while-for/CaratteriSpaziatura.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/*
+ * 	8- Si scriva un ciclo che conti il numero di caratteri di spaziattura in una stringa data.
+ */
+
+public class CaratteriSpaziatura {
+
+	public static void main(String[] args) {
+		
+		Scanner tastiera = new Scanner(System.in);
+		System.out.println("Inserire una frase e verranno calcolati i numeri di spazi al suo interno.");
+		String frase = tastiera.nextLine();
+		int numeroSpazi = 0;
+		
+		for(int i = 0; i < frase.length(); i++) {
+			
+			char spazio = frase.charAt(i);	
+			
+			if(spazio ==  ' ') {	
+				numeroSpazi ++;
+				
+			}
+		}
+		System.out.println("Il numero di spazi totali è: " + numeroSpazi);
+		
+	}
+
+}
