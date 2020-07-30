0e7f07b51a71e8ee24127d551e72b3470712874e
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-07-30 16:06:52 +0200

    Add files via upload

---
+++ Esercizi/Oggetti/CartaDiCreditoProva.java
@@ -0,0 +1,15 @@
+package oggetti;
+
+/*
+ * Test della classe CartaDiCredito
+ */
+
+public class CartaDiCreditoProva {
+
+	public static void main(String[] args) {
+		
+		CartaDiCredito nuovaCarta = new CartaDiCredito();
+		nuovaCarta.inizializzazione();
+	}
+
+}
