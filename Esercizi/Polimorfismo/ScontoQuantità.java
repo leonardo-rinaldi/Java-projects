75e6279834a382bee8db681e73f99129bfcad3d0
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-09-14 15:20:04 +0200

    Add files via upload

---
+++ Esercizi/Polimorfismo/ScontoQuantità.java
@@ -0,0 +1,34 @@
+/*
+ *	CAPITOLO 11
+ *
+ * 	4- Si derivi una classe astratta ScontoQuantità da PoliticaSconto, come descritta
+ * 	   nell'esercizio 3. Essa dovrebbe avere un costruttore con due parametri, minimo
+ * 	   e percentuale. Si dovrebbe ridefinire il metodo calcolaSconto in modo che se la
+ * 	   la quanità di un articolo acquistato è maggiore del minimo, lo sconto è di
+ * 	   percentuale sul totale.
+ * 
+ */
+
+public class ScontoQuantità extends PoliticaSconto {
+	
+	private double minimo;
+	private double percentuale;
+	
+	public ScontoQuantità(double minimo, double percentuale) {
+		this.minimo = minimo;
+		this.percentuale = percentuale;
+	}
+	
+	public double calcolaSconto() {
+		double sconto = 0;
+		if(getNumeroArticoli() > minimo) {
+			sconto = getNumeroArticoli() * getPrezzoArticolo() * percentuale;
+		}
+		return sconto;
+	}
+	
+	public double totale() {
+		double totale = getNumeroArticoli() * getPrezzoArticolo();
+		return totale - calcolaSconto();
+	}
+}
