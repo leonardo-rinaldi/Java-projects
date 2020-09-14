b69cfb562152fc726fca3a0c6841f88d1275c3fc
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-09-14 15:18:40 +0200

    Add files via upload

---
+++ Esercizi/Polimorfismo/PoliticaSconto.java
@@ -0,0 +1,25 @@
+/*
+ *	CAPITOLO 11
+ *
+ *	3- Si crei una classe astratta PoliticaSconto. Essa dovrebbe avere un solo metodo
+ *	   astratto calcolaSconto che restituirà lo sconto per l'acquisto di un certo numero
+ *	   di articoli tutti dello stesso tipo. Il metodo ha due parametri, numeroArticoli e
+ *	   prezzoArticolo.
+ *
+ */
+
+public abstract class PoliticaSconto {
+
+	private int numeroArticoli;
+	private double prezzoArticolo;
+	
+	public abstract double calcolaSconto();
+	
+	public int getNumeroArticoli() {
+		return numeroArticoli;
+	}
+	
+	public double getPrezzoArticolo() {
+		return prezzoArticolo;
+	}
+}
