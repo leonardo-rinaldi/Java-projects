b4c6df01c8e64638bf4cbcab374a9e5457abb6ba
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-09-15 15:06:39 +0200

    Add files via upload

---
+++ Esercizi/Polimorfismo/Esercizio-7/PoliticaScontoInterfaccia.java
@@ -0,0 +1,10 @@
+/*
+ * 	7- Si definisca PoliticaSconto come un'interfaccia invece che come la classe astratta
+ * 	   descritta nell'Esercizio 3.
+ * 
+ */
+
+public interface PoliticaScontoInterfaccia {
+
+	public double calcolaSconto();
+}
---
+++ Esercizi/Polimorfismo/Esercizio-8/CodificatoreMessaggio.java
@@ -0,0 +1,11 @@
+/*
+ *	8- Si crei un'interfaccia CodificaMessaggio che ha un solo metodo
+ *	   codifica(testoInChiaro), dove testoInChiaro sarà il messaggio
+ *	   da codificare. Il metodo restituirà il messaggio codificato.
+ * 
+ */
+
+public interface CodificatoreMessaggio {
+
+	public String codifica(String testoInChiaro);
+}
